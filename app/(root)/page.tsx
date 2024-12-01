262d0a84b083be4402311f61bb6be0f0b486f61a
dzmitrybeinia <dmitribeinia@gmail.com>
2024-12-01 09:19:23 +0100

    startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,25 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}: {
     searchParams: Promise<{ query?: string }>
 }) {
     const query = (await searchParams).query;
+
+    const posts = [{
+        _createdAt: new Date(),
+        _id: "1",
+        views: 100,
+        description: "A platform for developers to showcase their projects and get feedback.",
+        author: {
+            _id: "1",
+            name: "John Doe"
+        },
+        image: 'https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D',
+        title: "DevHub",
+        category: "Tech"
+    }];
+
     return (
         <>
             <section className={"pink_container"}>
@@ -12,6 +28,24 @@ export default async function Home({searchParams}: {
                     Submit your startup idea and get a chance to pitch it to investors.
                 </p>
                 <SearchForm query={query}/>
+            </section>
+
+            <section className={"section_container"}>
+                <p className={"text-30-semibold"}>
+                    {query ? (`Search results for "${query}"`) : ("All Startups")}
+                </p>
+
+                <ul className={"mt-7 card_grid"}>
+                    {posts?.length > 0 ?
+                        (
+                            posts.map((post: StatupCardType, index: number) => (
+                                <StartupCard key={post?._id} post={post}/>
+                            ))
+                        ) : (
+                            <p className={"no-result"}>No startups found</p>
+                        )
+                    }
+                </ul>
             </section>
         </>
     );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDateTime(date: string) {
+  return new Date(date).toLocaleString(
+      'en-US',
+        {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+        }
+  )
+}
