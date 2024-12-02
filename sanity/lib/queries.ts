d8c4755982ed00a6b9100213fe0e905d58e71b75
dzmitrybeinia <dmitribeinia@gmail.com>
2024-12-02 10:05:44 +0100

    real-time search w/ url query params

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,8 @@
 import {defineQuery} from "groq";
 
 export const STARTUPS_QUERY =
-    defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {
+    defineQuery(`*[_type == "startup" && defined(slug.current) && 
+    !defined($search) || category match $search || title match $search || author->name match $search] | order(_createdAt desc) {
     _id, 
     title,
     slug,
