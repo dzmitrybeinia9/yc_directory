412df87fb396e6e4c515b9eae0ce7d34c0125519
dzmitrybeinia <dmitribeinia@gmail.com>
2024-12-03 10:46:05 +0100

    update views

--- components/View.tsx
+++ components/View.tsx
@@ -2,10 +2,19 @@ import React from 'react'
 import Ping from "@/components/Ping";
 import {STARTUP_VIEWS_QUERY} from "@/sanity/lib/queries";
 import {client} from "@/sanity/lib/client";
+import {writeClient} from "@/sanity/lib/write-client";
 
 const View = async ({id}: { id: string }) => {
-    const {views: totalViews} = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY, {id});
-    console.log(totalViews);
+    const {views: totalViews} = await client
+        .withConfig({useCdn: false})
+        .fetch(STARTUP_VIEWS_QUERY, {id});
+
+    await writeClient
+        .patch(id)
+        .setIfMissing({views: 0})
+        .inc({views: 1})
+        .commit();
+
     return (
         <div className={"view-container"}>
             <div className={"absolute -top-2 -right-2"}>
